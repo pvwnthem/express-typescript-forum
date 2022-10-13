01a50caf7e3aa88f0a9701900aab45fd8ed4f077
pvwnthem <pvwn.them@gmail.com>
2022-10-13 00:52:02 -0400

    add session and passport

--- server.ts
+++ server.ts
@@ -6,13 +6,23 @@ import auth from './routes/auth';
 import api from './routes/api';
 import ejs from 'ejs'
 import * as bodyParser from 'body-parser';
+import session from 'express-session';
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'views')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
+app.use(passport.initialize());
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        secure: true,
+    }
+   
+}));
 const port: Number = 8080;
 
 app.use('/', index);
