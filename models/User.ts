862f5a25706a7eaeecb4fdd499fbd12ff2d6b55e
pvwnthem <pvwn.them@gmail.com>
2022-10-14 00:28:04 -0400

    add /user/:username route and prepare for making a user page

--- models/User.ts
+++ models/User.ts
@@ -49,6 +49,10 @@ const UserSchema = new mongoose.Schema({
     isEmailShown: {
       type: Boolean,
       default: false
+    },
+    private: {
+      type: Boolean,
+      default: false
     }
     
   });
--- routes/index.ts
+++ routes/index.ts
@@ -1,5 +1,16 @@
 import express from 'express';
 const router = express.Router();
+import User from '../models/User'
+import dburl from '../config/keys'
+
+const db: string = dburl.dburl
+
+import mongoose from 'mongoose';
+mongoose.connect(db).then(() => {
+    console.log('mongodb connection established');
+  }).catch(err => {
+    console.log(err);
+  });
 
 router.get('/', (req, res) => {
     res.send('index')
@@ -10,6 +21,19 @@ router.get('/login', (req, res) => {
 router.get('/user/:username', (req, res) => {
     console.log(req.params.username);
     res.send(req.params.username);
+    const username: string = req.params.username;
+    User.findOne({ username: username }, (err: any, user: any) => {
+        if (!user.private) {
+            if (!user.isEmailShown == false){
+                res.render('userpage', {username: user.name, email: 'this users email is private', followers: user.follwers, following: user.following, private: false })
+            } else if (user.isEmailShown == true) {
+                res.render('userpage', {username: user.name, email: user.email, followers: user.follwers, following: user.following, private: false})
+            }
+        } else {
+            res.render('userpage', {username: user.name, email: 'this users email is private', followers: user.follwers, following: user.following, private: true })
+        }
+        
+    })
 })
 
 export default router;
